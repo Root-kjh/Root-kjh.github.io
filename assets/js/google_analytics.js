3a80e816375c706d54c889f95c8deaa551ed24b9
DrK <wldnro3141@gmail.com>
2020-01-16 14:49:36 +0900

    google analytics 오류 수정

--- assets/js/google_analytics.js
+++
@@ -1,6 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156322128-1"></script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-156322128-1');
